0ddaab5ac47ab2ddd8716473aa33f9b4d2496730
Dmitry Sidorenko <93076069+DmitrySidorenko53@users.noreply.github.com>
2022-02-28 17:07:41 +0300

    Update Main.java

--- src/by/epam/jwd/sidorenko/Main.java
+++ src/by/epam/jwd/sidorenko/Main.java
@@ -5,6 +5,7 @@ import by.epam.jwd.sidorenko.launch.FunctionController;
 import java.util.Scanner;
 
 public class Main {
+    @SuppressWarnings("resourse")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         FunctionController.run(scanner.nextLine());
